687dbc922a80bc545d0f5136248efcd4a6226c23
yeaungzaw <yeaungzaw.learner@gmail.com>
2024-06-04 10:27:03 +0630

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Button>Hello (React + Shadcn + React Router Dom)</Button>
+    </div>
+  );
+};
+
+export default App;
